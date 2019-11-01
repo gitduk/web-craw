f783aa4c524928fc1eec2997345f049dff04eefd
gitduk <wdkany@gmail.com>
2019-11-01 19:00:01 +0800

    up   2019年 11月 01日 星期五 19:00:01 CST

---
+++ linux/software_install_list/0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./fish.install
+./apt-fast.install
+./autojump.install
+./fzf.install
+./java13.install
+./maven.install
+./omf.install
+./pip.install
+./Preload.install
+./TLP.install
+./tmux.install
+./tweak.install
---
+++ python/pl.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+im = Image.open('/home/dongkai/Pictures/p1.jpeg')
+
+im.show()
+
+
