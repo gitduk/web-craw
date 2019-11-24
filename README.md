89cfb86e2e60d7d979a7e91dd1351c45831b1c78
gitduk <wdkany@gmail.com>
2019-11-24 17:14:11 +0800

    look readme   2019年 11月 24日 星期日 17:14:11 CST

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mycode
+
+## getwallpaper.py
+
+功能：多线程爬取[ZOL桌面壁纸](http://desk.zol.com.cn/ "wallpaper")上的壁纸,并自动分类。
+
+可自选爬取壁纸的种类，页数(线程)
+
+页面展示：
---
+++ python/day2.py
@@ -0,0 +1,24 @@
+list:
+    切片赋值，插入, 删除
+
+    append
+    extend
+    insert
+
+    del
+    remove
+    clear
+    pop
+
+    count
+    index
+
+    reverse
+    sort
+    就地修改
+    sorted
+
+    copy
+
+元组
+    tuple
